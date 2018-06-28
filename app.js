192dfbb36bdc6813ce6af6ec2e6772482e44394a
hab278 <hab278@nyu.edu>
2018-06-27 23:39:31 -0400

    Clean up code to retrieve poll by id

--- app.js
+++ app.js
@@ -174,18 +174,20 @@ router.post('/polls', function(req, res) {
 });
 
 router.get('/polls/:id', function(req, res) {
-  Poll.findById(parseInt(req.params["id"])).then(poll =>{
+  Poll.find({
+    where: {
+      id: parseInt(req.params["id"])
+    },
+    attributes: [
+      ["id", "poll_id"],
+      "question",
+      "answers",
+      "participant_count",
+      "price"
+    ]
+  }).then(poll =>{
     if(poll) {
-      Result.findAndCountAll({ where: { poll_id: poll['id'] } }).then( results => {
-        var json_out = {
-          poll_id: poll['id'],
-          question: poll['question'],
-          answers: poll['answers'],
-          participant_count: results.count,
-          price: poll["price"]
-        }
-        res.json(json_out);
-      })
+      res.json(poll);
     } else {
       res.status(404).json({ error: "Not Found", message: "Poll not found"})
     }
