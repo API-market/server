d7fef650697e84dea9449725d554375ae6f2255a
Zokir Tiliaev <zakirenish@gmail.com>
2018-07-15 00:55:23 -0400

    Added test execution framework.

---
+++ tests/runner.js
@@ -0,0 +1,27 @@
+var newman = require('newman')
+var request = require('supertest');
+const fs = require('fs');
+
+const SQLITE_FILENAME = process.env.SQLITE_FILENAME;
+
+describe('loading express', function () {
+  var server;
+  beforeEach(function () {
+    if (fs.existsSync(SQLITE_FILENAME)) {
+      fs.unlink(SQLITE_FILENAME, (err) => {
+            if (err) throw err;
+              console.log(SQLITE_FILENAME + ' was deleted');
+      });
+    }     
+    server = require('../server');
+  });
+  afterEach(function () {
+    server.close();
+  });
+  it('simple_test', function simpleTest(done) {
+      newman.run({
+              collection: 'tests/data/simple_test.postman_collection.json',
+                  reporters: ['cli']
+      }, process.exit);
+  });
+});
