1200b3ea55ab3e0eb359e77081e758b991cb87f3
yaromir-technorely <yaromir.kozak@technorely.com>
2018-09-20 12:44:57 +0300

    LUB-440 LUB-441 Send message and verify phone

--- services/index.js
+++ services/index.js
@@ -4,11 +4,6 @@ exports.UploadService = require('./upload_service');
 exports.PushService = require('./push_service');
 exports.PollService = require('./poll_service');
 exports.MailService = require('./mail_service');
-
+exports.MessageService = require('./message_service');
 
 exports.mailService = new exports.MailService();
-// exports.uploadService = new exports.UploadService();
-// exports.pushService = new PushService;
-// exports.pollService = new PollService;
-
-// exports.pollService.getNotAnswersPull().then(console.log);
