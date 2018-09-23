a475b53426c715e2748991f3c0f8c56a95d3f35f
yaromir-technorely <yaromir.kozak@technorely.com>
2018-09-23 20:22:17 +0300

    LUB-463 Fix message

--- user_routes.js
+++ user_routes.js
@@ -843,7 +843,7 @@ userRouter
         })
             .then(function (user) {
                 if (!user) {
-                    throw new Error('User not found');
+                    throw new Error('User was verify');
                 }
                 return user.update(model.formattingValue({
                     verifyToken: null,
