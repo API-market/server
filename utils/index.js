d1e75fe016d1a95e85920df96e46735d0d36eee4
yaromir-technorely <yaromir.kozak@technorely.com>
2018-09-26 13:56:02 +0300

    LUB-494 WIP create migrate and view

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,9 @@
 const Events = require('./events');
 const Token = require('./token');
 const Model = require('./model');
+const Migration = require('./migration');
 
 exports.events = new Events;
 exports.token = new Token;
 exports.model = new Model;
+exports.migration = new Migration;
