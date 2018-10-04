140b9ca4225f997f2c181d0980fb3132216131ec
Germis <al.germis@gmail.com>
2018-10-04 11:33:43 +0300

    Fix unchained promises race while seeding

--- seeders/20180920103011-create-users-for-custom-notifications.js
+++ seeders/20180920103011-create-users-for-custom-notifications.js
@@ -1,16 +1,16 @@
 'use strict';
 
 const serverInfo = require('../server_info.js');
-const {ProfileImage, User} = require('../db_entities')
+const {ProfileImage, User} = require('../db_entities');
 const SEED_AUTH = serverInfo.SEED_AUTH;
 const USER_MAIN_LUMEOS = serverInfo.USER_MAIN_LUMEOS;
 
 module.exports = {
     up: (queryInterface, Sequelize) => {
-        return Promise.all([
-            queryInterface.bulkDelete('users', {
+        return queryInterface.bulkDelete('users', {
                 email: USER_MAIN_LUMEOS
-            }, {}),
+            }, {})
+		.then(() =>
             queryInterface.bulkInsert('users', [
                 {
                     lastName: 'Notification',
@@ -21,24 +21,27 @@ module.exports = {
                     updatedAt: new Date(),
 
                 },
-            ]),
-            User.findOne({
-                where: {
-                    email: USER_MAIN_LUMEOS
-                }
-            }).then((user) => {
-                return ProfileImage.destroy({
-                    where: {
-                        image: 'd335853c84e28984997b7c7e_logo.png'
-                    }
-                }).then(() => {
-                    return ProfileImage.upsert({
-                        user_id: user.id,
-                        image: 'd335853c84e28984997b7c7e_logo.png'
-                    })
-                })
-            })
-        ]);
+            ])
+		)
+		.then(() => User.findOne({
+				where: {
+					email: USER_MAIN_LUMEOS
+				}
+			}).then((user) => {
+				return ProfileImage.destroy({
+					where: {
+						image: 'd335853c84e28984997b7c7e_logo.png'
+					}
+				}).then(() => {
+					return ProfileImage.upsert({
+						user_id: user.id,
+						image: 'd335853c84e28984997b7c7e_logo.png'
+					})
+				})
+			})
+		)
+		.catch(console.error);
+
     },
 
     down: (queryInterface, Sequelize) => {
