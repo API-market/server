e18bedd9399587d852bdd1c5657385ad671180a9
Germis <al.germis@gmail.com>
2018-10-08 19:00:15 +0300

    LUB-608 Make unable to edit poll after someone participated

--- controllers/community_polls_controller.js
+++ controllers/community_polls_controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const {communityPolls, community, sequelize} = require('lumeos_models');
+const {communityPolls, community, pollAnswers, sequelize} = require('lumeos_models');
 const {UploadS3Service} = require('lumeos_services');
 const {errors} = require('lumeos_utils');
 
@@ -72,45 +72,60 @@ class CommunityPollsController {
 
     update(req, res, next) {
         const {poll_id} = req.params;
+        let community;
+
         return sequelize.transaction((transaction) => {
             return communityPolls.findById(poll_id)
-                .then((communityPollsEntity) => {
-                    if (!communityPollsEntity) {
-                        throw errors.notFound();
-                    }
-                    if (req.auth.user_id !== communityPollsEntity.creator_id) {
-                        throw errors.forbidden('This community not yours');
-                    }
-                    let oldImage = communityPollsEntity.image;
-                    return communityPollsEntity.update(communityPolls.formatData(req.body), {transaction})
-                        .then((CommunitiesPollsUpdated) => {
-                            return UploadS3Service
-                                .upload(req.body.image, 'community_polls')
-                                .then(({file}) => {
-                                    if (!file) {
-                                        oldImage = null;
-                                        return CommunitiesPollsUpdated;
-                                    }
-                                    return CommunitiesPollsUpdated
-                                        .update({image: file}, {transaction})
-                                        .then((CommunitiesPollsUpdated) => {
-                                            return CommunitiesPollsUpdated;
-                                        });
-                                });
-                        })
-                        .then((CommunitiesPollsUpdated) => {
-                            if (!oldImage)
-                                return res.sendResponse(communityPolls.formatResponse(CommunitiesPollsUpdated));
+			.then(communityPollsEntity => {
+				if (!communityPollsEntity) {
+					throw errors.notFound();
+				}
+				if (req.auth.user_id !== communityPollsEntity.creator_id) {
+					throw errors.forbidden('This community not yours');
+				}
 
-                            return UploadS3Service
-                                .delete(oldImage)
-                                .then(() => {
-                                    return res.sendResponse(communityPolls.formatResponse(CommunitiesPollsUpdated));
-                                });
-                        });
-                });
+				community = communityPollsEntity;
+				return communityPollsEntity
+			})
+			.then(communityPollsEntity => pollAnswers.count({ where: {'poll_id': poll_id} }))
+			.then(pollAnswersCount => {
+				if (pollAnswersCount > 0) {
+					throw errors.forbidden('You can`t edit poll. Someone already participated');
+				}
+
+				return community;
+			})
+			.then((communityPollsEntity) => {
+				let oldImage = communityPollsEntity.image;
+				return communityPollsEntity.update(communityPolls.formatData(req.body), {transaction})
+				.then((CommunitiesPollsUpdated) => {
+					return UploadS3Service
+					.upload(req.body.image, 'community_polls')
+					.then(({file}) => {
+						if(!file){
+							oldImage = null;
+							return CommunitiesPollsUpdated;
+						}
+						return CommunitiesPollsUpdated
+						.update({image: file}, {transaction})
+						.then((CommunitiesPollsUpdated) => {
+							return CommunitiesPollsUpdated;
+						});
+					});
+				})
+				.then((CommunitiesPollsUpdated) => {
+					if(!oldImage)
+						return res.sendResponse(communityPolls.formatResponse(CommunitiesPollsUpdated));
+
+					return UploadS3Service
+					.delete(oldImage)
+					.then(() => {
+						return res.sendResponse(communityPolls.formatResponse(CommunitiesPollsUpdated));
+					});
+				});
+			});
         })
-            .catch(next);
+		.catch(next);
     }
 }
 
