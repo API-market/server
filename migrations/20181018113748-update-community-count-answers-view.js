1f12e39dd5063273c1072f00037422b65b5af0c8
yaromir-technorely <yaromir.kozak@technorely.com>
2018-10-19 11:43:31 +0300

    Fix: Migration for test (ref LUB-702)

--- migrations/20181018113748-update-community-count-answers-view.js
+++ migrations/20181018113748-update-community-count-answers-view.js
@@ -6,7 +6,7 @@ const {community_count_answers_updated, community_count_answers} = require('../m
 const schemaName = db.schemaNames.communities;
 module.exports = {
     up: (queryInterface, Sequelize) => {
-        return migration.dropView(queryInterface, `community_count_answers`, community_count_answers_updated(), {schema: schemaName}).then(() => {
+        return migration.dropView(queryInterface, `community_count_answers`, '', {schema: schemaName}).then(() => {
             return migration.createView(queryInterface, `community_count_answers`, community_count_answers_updated(), {schema: schemaName});
         });
     },
