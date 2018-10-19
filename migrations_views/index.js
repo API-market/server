a85074ab15fb2cf165ecb2f28af10d910ee3a723
yaromir-technorely <yaromir.kozak@technorely.com>
2018-10-19 11:12:25 +0300

    Update: Change logic for community count answers (ref LUB-702)

--- migrations_views/index.js
+++ migrations_views/index.js
@@ -51,19 +51,19 @@ exports[exports.constansts.communityCountAnswers]= () => {
 };
 
 exports[exports.constansts.communityCountAnswersUpdated]= () => {
-	return `(
+    return `(
         SELECT
-          c_ct.id                                     AS community_id,
+          c_ct.id,
           count((SELECT count(c_pa.poll_id)
                  FROM communities.poll_answers c_pa
                  WHERE c_pa.poll_id = c_pl.id
                  LIMIT 1))                            AS count_answers,
           rank()
           OVER (
-            ORDER BY (count(DISTINCT c_pl.id)) DESC ) AS rank
+            ORDER BY (count(DISTINCT c_pl.id)) DESC) AS rank
         FROM communities.polls c_pl
           JOIN communities.community c_ct ON ((c_ct.id = c_pl.community_id))
-        GROUP BY c_ct.id))`;
+        GROUP BY c_ct.id)`;
 };
 
 exports[exports.constansts.communityCountPolls]= () => {
