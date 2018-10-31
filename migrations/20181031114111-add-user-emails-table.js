bb3aa08e43d18f9628ebd3ec8cea05ae43b0ee01
Germis <al.germis@gmail.com>
2018-10-31 14:00:12 +0200

    LUB-761 Add necessary models for user emails

---
+++ migrations/20181031114111-add-user-emails-table.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.createTable('user_emails', {
+
+			id: {
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+
+			userId: {type: Sequelize.INTEGER, allowNull: false},
+
+			email: {type: Sequelize.STRING, allowNull: false, unique: true},
+			name: {type: Sequelize.STRING, allowNull: true},
+			type: {type: Sequelize.STRING, allowNull: false},
+
+			verify: {type: Sequelize.BOOLEAN, allowNull: false, default: false},
+			verify_token: {type: Sequelize.STRING},
+
+			createdAt: {type: Sequelize.DATE},
+			updatedAt: {type: Sequelize.DATE},
+			deletedAt: {type: Sequelize.DATE, allowNull: true},
+
+		});
+	},
+
+	down: (queryInterface, Sequelize) => {
+		return queryInterface.dropTable('user_emails', {});
+	}
+};
---
+++ models/userEmails.js
@@ -0,0 +1,61 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+	const UserEmail = sequelize.define('userEmail', {
+
+		id: {
+			autoIncrement: true,
+			primaryKey: true,
+			type: DataTypes.INTEGER
+		},
+
+		userId: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+
+		email: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			unique: true
+		},
+
+		name: {
+			type: DataTypes.STRING,
+			allowNull: true,
+		},
+
+		type: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+
+		verify: {type: DataTypes.BOOLEAN, defaultValue: false},
+		verify_token: DataTypes.STRING,
+
+		createdAt: {
+			type: DataTypes.DATE,
+			defaultValue: new Date()
+		},
+		updatedAt: {
+			type: DataTypes.DATE,
+			defaultValue: new Date()
+		},
+
+	}, {
+		tableName: 'user_emails',
+		timestamps: true,
+		updatedAt: 'updatedAt',
+		createdAt: 'createdAt',
+		paranoid: true,
+	});
+
+	UserEmail.associate = function (models) {
+		UserEmail.belongsTo(models.users, {as: 'user', foreignKey: 'user_id'});
+	};
+
+	UserEmail.methods = (models, _, db) => {
+	};
+
+	return UserEmail;
+};
